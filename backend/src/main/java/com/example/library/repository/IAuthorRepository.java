2eb1fce76fdc2c15abca2d2e82d0ca552dcebe0c
Popchiyk <roma240103@gmail.com>
2022-08-31 14:02:50 +0300

    branch frontend

--- backend/src/main/java/com/example/library/repository/IAuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.library.repository;
-
-import com.example.library.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAuthorRepository extends JpaRepository<Author, Integer> {
-}
